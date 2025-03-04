411ab02fbd32d9d6db1b6855fc3627a8ace8594f
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-04 12:46:29 -0300

    feat: wrap application in HelmetProvider for SEO optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
@@ -6,14 +7,16 @@ import MyRecipes from './pages/MyRecipes.jsx';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={'/'} element={<Home />}></Route>
-        <Route path={'/login'} element={<Login />}></Route>
-        <Route path={'/register'} element={<Register />}></Route>
-        <Route path={'/recipes'} element={<MyRecipes />}></Route>
-      </Routes>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <Routes>
+          <Route path={'/'} element={<Home />}></Route>
+          <Route path={'/login'} element={<Login />}></Route>
+          <Route path={'/register'} element={<Register />}></Route>
+          <Route path={'/recipes'} element={<MyRecipes />}></Route>
+        </Routes>
+      </Router>
+    </HelmetProvider>
   );
 }
 
