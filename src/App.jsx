5b4bcd53c6c3248d54f25ee449b189bef0f5a0dd
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-04 12:25:22 -0300

    feat: implement routing with React Router and replace ComingSoon with Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import ComingSoon from './pages/ComingSoon.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import MyRecipes from './pages/MyRecipes.jsx';
 
 function App() {
   return (
-    <>
-      <ComingSoon />
-    </>
+    <Router>
+      <Routes>
+        <Route path={'/'} element={<Home />}></Route>
+        <Route path={'/login'} element={<Login />}></Route>
+        <Route path={'/register'} element={<Register />}></Route>
+        <Route path={'/recipes'} element={<MyRecipes />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
