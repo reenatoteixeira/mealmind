b10e8c8f905738ddc2cf0fc8afcc1675484b5d33
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-04 12:24:47 -0300

    feat: rename ComingSoon component to Home and update content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Header from '../components/Header.jsx';
 import comingSoonGIF from '../assets/images/coming-soon.gif';
 
-function ComingSoon() {
+function Home() {
   return (
     <>
       <Header />
@@ -9,6 +9,8 @@ function ComingSoon() {
       <section className={'min-h-screen bg-stone-100 text-stone-950 text-center flex items-center justify-center'}>
         <div className={'container flex flex-col justify-center items-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'}>
           <img src={comingSoonGIF} alt={'GIF showing an animated "Coming Soon" text.'} className={'w-1/2'} />
+
+          <p className={'mb-4 text-stone-300'}>You're in the 'Home' page</p>
 
           <h1 className={'text-xl font-bold text-red-600 md:text-3xl'}>
             Your Smart Recipe Organizer is Cooking! </h1>
@@ -21,7 +23,7 @@ function ComingSoon() {
             Stay tuned! MealMind is coming soon with delicious features. </p>
 
           <p className={'opacity-50 text-sm md:text-lg'}>
-            In the meantime, follow updates on our
+            In the meantime, follow updates on our {' '}
             <a href={'https://github.com/reenatoteixeira/mealmind'} className={'text-red-600 underline'}>
               GitHub
             </a>
@@ -32,4 +34,4 @@ function ComingSoon() {
   );
 }
 
-export default ComingSoon;
+export default Home;
