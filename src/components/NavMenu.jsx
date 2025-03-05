9abc6891622f309f164dc98ddf71242db7cb33b5
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-05 16:18:42 -0300

    style: improve button and link styles in navigation menus for better accessibility and visual consistency

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -9,14 +9,14 @@ function NavMenu(props) {
       <div className={'hidden md:flex md:items-center md:justify-between md:w-full'}>
         <ul className={'flex gap-6 text-lg'}>
           <li>
-            <Link to={'/'} className={'transition-all hover:text-red-600'}>
+            <Link to={'/'} className={'transition-all hover:text-red-600 hover:underline'}>
               Feed
             </Link>
           </li>
           {props.user ? (
             <>
               <li>
-                <Link to={'/recipes'} className={'transition-all hover:text-red-600'}>
+                <Link to={'/recipes'} className={'transition-all hover:text-red-600 hover:underline'}>
                   My Recipes
                 </Link>
               </li>
@@ -31,19 +31,19 @@ function NavMenu(props) {
                 Hi, {props.user.displayName}! </p>
 
               <button onClick={logout}
-                      className={'text-lg px-4 py-1 border-2 border-red-600 text-red-600 rounded-lg transition-all cursor-pointer hover:bg-red-100'}>
+                      className={'transition-all cursor-pointer rounded-md px-3.5 py-1 text-lg text-red-600 border-2 border-red-600 shadow-xs hover:bg-red-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'}>
                 <i className={'uil uil-signout'}></i> Log out
               </button>
             </>
           ) : (
             <>
               <Link to={'/login'}
-                    className={'text-lg px-4 py-1 border-2 border-red-600 text-red-600 rounded-lg transition-all hover:bg-red-100'}>
+                    className={'transition-all cursor-pointer rounded-md px-3.5 py-1 text-lg text-red-600 border-2 border-red-600 shadow-xs hover:bg-red-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'}>
                 Sign in
               </Link>
 
               <Link to={'/register'}
-                    className={'text-lg px-4 py-1 border-2 border-red-600 bg-red-600 text-stone-100 rounded-lg transition-all hover:bg-red-500'}>
+                    className={'transition-all cursor-pointer rounded-md bg-red-600 px-3.5 py-1 text-lg text-stone-100 border-2 border-red-600 shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'}>
                 Sign up
               </Link>
             </>
