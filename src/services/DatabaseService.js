8d58f63daaedac73706196466f683f768a50aab6
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-06 19:41:39 -0300

    feat: add DatabaseService for handling Firestore document retrieval

---
+++ src/services/DatabaseService.js
@@ -0,0 +1,26 @@
+import { getDocs, collection, query, where } from 'firebase/firestore';
+import { db } from './firebase.js';
+
+class DatabaseService {
+  constructor() {
+    this.db = db;
+  }
+
+  async getDocuments(collectionName) {
+    return await getDocs(collection(this.db, collectionName))
+      .then((querySnapshot) => {
+        return querySnapshot.docs.map(doc => (doc.data()));
+      });
+  }
+
+  async getDocumentsByAttribute(collectionName, documentAttribute, operator, value) {
+    const q = query(collection(this.db, collectionName), where(documentAttribute, operator, value));
+
+    return await getDocs(q)
+      .then((querySnapshot) => {
+        return querySnapshot.docs.map(doc => (doc.data()));
+      });
+  }
+}
+
+export const DATABASE_SERVICE = new DatabaseService();
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,6 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig),
-  auth = getAuth(app);
+  auth = getAuth(app),
+  db = getFirestore(app);
 
-export { app, auth };
+export { app, auth, db };
