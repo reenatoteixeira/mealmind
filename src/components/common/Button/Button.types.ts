ade28770a367b99bc219e5cc0f179609660e7465
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-20 17:56:42 -0300

    feat: refactor Button component to use centralized styling and add ariaLabel prop

--- src/components/common/Button/Button.types.ts
+++ src/components/common/Button/Button.types.ts
@@ -1,12 +1,9 @@
+import { ButtonState, ButtonStyle, ButtonSize, IconPosition } from '../../../types.ts';
 import { LucideIcon } from 'lucide-react';
-
-export type ButtonSize = 'sm' | 'md' | 'lg' | 'xl';
-export type ButtonStyle = 'primary' | 'secondary';
-export type ButtonState = 'enabled' | 'disabled';
-export type IconPosition = 'left' | 'right' | 'alone';
 
 export interface ButtonProps {
   label?: string;
+  ariaLabel?: string;
   size?: ButtonSize;
   style?: ButtonStyle;
   icon?: LucideIcon;
